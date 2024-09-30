f3c7c5499d4941439807e4e71c52be8c5c4ed937
Angelo Netho <angelonetho@gmail.com>
2024-09-30 00:12:11 -0300

    finalize collection content

--- src/pages/Collection/index.tsx
+++ src/pages/Collection/index.tsx
@@ -7,80 +7,104 @@ export function Collection() {
     return (
         <BaseWindow title="Minha Coleção" variant="orange" close_to="/">
             <Title variant="orange">Minha Coleção</Title>
-            <ImageWithText src="/smart.jpg" description="Descrição da imagem"/>
             <Paragraph>
                 Eu já te contei como é a minha vida? <br/><br/>
 
-                Eu tenho 19 anos, faço 20 em janeiro do ano que vem. Nasci em Curitiba mas morei toda minha vida em Paranaguá, no litoral do Paraná. Isso até eu me mudar para Curitiba com o meu irmão que já estava na faculdade.<br/><br/>
+                Eu tenho 19 anos e faço 20 em janeiro do ano que vem. Nasci em Curitiba, mas morei toda a minha vida em Paranaguá, no litoral do Paraná. Isso até eu me mudar para Curitiba com o meu irmão, que já estava na faculdade.<br/><br/>
 
-                Mas eu vou pular essas informações que tem na minha identidade e mostrar coisas, coisas aleatórias, que eu acho dahora, que eu fiz na minha vida.<br/><br/>
+                Mas eu vou pular essas informações que estão na minha identidade e mostrar coisas, coisas aleatórias, que eu acho da hora e que eu fiz na minha vida.<br/><br/>
 
-                Desculpa se eu escrever muito (eu gosto de escrever muito).
+                Desculpa se eu escrever muito (eu gosto de escrever bastante).
             </Paragraph>
             <Title variant="orange">Minecraft</Title>
             <Paragraph>
-                Se você assistiu meu vídeo, ele começa falando que a minha jornada começou no Minecraft. E de fato, foi lá mesmo, e embora eu tenha expandido para outras áreas fora do jogo, eu ainda continuei desenvolvendo para o Minecraft. <br/><br/>
+                Se você assistiu ao meu vídeo, ele começa falando que a minha jornada começou no Minecraft. E de fato, foi lá mesmo. Embora eu tenha expandido para outras áreas fora do jogo, eu continuei desenvolvendo para o Minecraft. <br/><br/>
 
-                Eu era fascinado por servidores, a experiência do multijogador. Certo dia eu bati a mão na mesa e disse "Como assim eles conseguem fazer isso? Eu também quero fazer igual eles". <br/><br/>
+                Eu era fascinado por servidores, pela experiência do multijogador. Certo dia eu bati a mão na mesa e disse: "Como assim eles conseguem fazer isso? Eu também quero fazer igual a eles." <br/><br/>
 
-                Após muita pesquisa, eu desisti umas 3 vezes em 1 ano, parecia coisa demais para cabeça de um pré adolescente de 11 anos, mas uma hora foi. <br/><br/>
+                Após muita pesquisa, eu desisti umas três vezes em um ano; parecia coisa demais para a cabeça de um pré-adolescente de 11 anos, mas uma hora deu certo. <br/><br/>
 
-                Tem duas coisas em específico que eu me orgulho que envolvem o Minecraft. A primeira é que eu abria servidores, todas as férias meus amigos me chamavam e perguntavam "Ei, vai ter servidor de Mine?", isso me deixava muito feliz. <br/><br/>
+                Tem duas coisas específicas das quais eu me orgulho que envolvem o Minecraft. A primeira é que eu abria servidores; todas as férias meus amigos me chamavam e perguntavam: "Ei, vai ter servidor de Mine?". Isso me deixava muito feliz. <br/><br/>
 
-                Essa tradição desde 2017 teve seu auge na pandemia, quando eu desenvolvia plugins para adicionar novas coisas no jogo, novos chefões, novas habilidades, novos NPCs e etc. <br/><br/>
+                Essa tradição começou em 2017 e teve seu auge na pandemia, quando eu desenvolvia plugins para adicionar novas coisas ao jogo, como novos chefões, novas habilidades, novos NPCs, etc. <br/><br/>
 
-                Por incrível que pareça eu não jogava com eles, eu ficava conversando com eles em chamada claro, mas dentro do jogo eu era uma entidade invisível, eu gostava apenas de observar meus amigos se divertindo e eu criando essa experiência para eles, eu criava por exemplos eventos aleatórios de chefões do nada e ver a empolgação deles era minha felicidade. <br/><br/> 
+                Por incrível que pareça, eu não jogava com eles; eu ficava conversando com eles em chamada, claro, mas dentro do jogo eu era uma entidade invisível. Eu gostava apenas de observar meus amigos se divertindo e de criar essa experiência para eles; por exemplo, eu criava eventos aleatórios de chefões, do nada, e ver a empolgação deles era a minha felicidade. <br/><br/> 
 
-                Justamente por eu desenvolver esses plugins para o servidor com meus amigos, eu notei que eu podia separar em módulos e publicar em um fórum público, assim outras pessoas poderiam ter acesso também. <br/><br/>
+                Justamente por desenvolver esses plugins para o servidor com meus amigos, eu notei que podia separá-los em módulos e publicar em um fórum público, assim outras pessoas poderiam ter acesso também. <br/><br/>
 
-                Um dos plugins que eu desenvolvi para um servidor de RPG, era de ter uma barra de vida em cima do jogador, e na época o jogo tinha acabado de atualizar, e o desenvolvedor que já tinha esse plugin, tinha sumido. <br/><br/><br/>
+                Um dos plugins que eu desenvolvi para um servidor de RPG adicionava uma barra de vida em cima do jogador, e na época o jogo tinha acabado de atualizar, e o desenvolvedor que já tinha esse plugin tinha sumido. <br/><br/><br/>
 
                 <ImageWithText src="/healthbar.webp" description="As barras de vida eram adicionadas acima da entidade."/>
 
-                Eu publiquei esse plugin e eu me impressionei, como uma ideia tão simples, uma programação tão simples serviu para tantas pessoas. <br/><br/>
+                Eu publiquei esse plugin e fiquei impressionado com como uma ideia tão simples, uma programação tão simples, serviu para tantas pessoas. <br/><br/>
 
-                Hoje esse projeto tem mais de 100 mil downloads nesse fórum. Quando eu fui em um show no Allianz Parque eu olhei aquela imensidão de gente e pensei "Cara, aqui só tem 50 mil, o dobro de todo esse tanto de gente baixou algo que eu fiz". <br/><br/>
+                Hoje esse projeto tem mais de 100 mil downloads nesse fórum. Quando fui a um show no Allianz Parque, eu olhei aquela imensidão de gente e pensei: "Cara, aqui só tem 50 mil, o dobro de todo esse tanto de gente baixou algo que eu fiz." <br/><br/>
                 
                 <ImageWithText src="/healthbar2.webp" description="Mais de 100 mil downloads no fórum do Spigot."/>
                 
-                Eu me orgulho muito desse projeto, ele me ensinou que as vezes a ideia mais simples, é o que todo mundo precisa. 
+                Eu me orgulho muito desse projeto; ele me ensinou que, às vezes, a ideia mais simples é o que todo mundo precisa. 
             </Paragraph>
 
             <Title variant="orange">Irmão</Title>
             <Paragraph>
-                Como eu disse, eu moro com meu irmão mais velho, e várias coisas que a gente faz no dia a dia é juntos.<br/><br/>
+                Como eu disse, eu moro com meu irmão mais velho, e várias coisas que a gente faz no dia a dia são juntos.<br/><br/>
 
-                Ir na academia agora é muito mais fácil com um motivando o outro, quando o tempo está bom a gente até corre no Parque Barigui.<br/><br/>
+                Ir à academia agora é muito mais fácil com um motivando o outro. Quando o tempo está bom, a gente até corre no Parque Barigui.<br/><br/>
 
-                Mas se tem alguma coisa que eu gosto de fazer com ele é assistir filmes e séries. É ele quem me introduz as coisas boas, ele que me mostrou os meus filmes favoritos.<br/><br/>
+                Mas se tem uma coisa que eu gosto de fazer com ele, é assistir a filmes e séries. É ele quem me introduz às coisas boas; foi ele que me mostrou os meus filmes favoritos.<br/><br/>
 
-                Todo final de semana eu desço para Paranaguá, pra matar a saudades da minha vó, minha tia e do meu pai (E do meu cachorro também).<br/><br/>
+                <ImageWithText src="/irmao.webp" description="  <-- Meu irmão é o da esquerda."/>
 
-                E a noite, a gente assiste alguma coisa, e pra isso eu geralmente faço uma pizza, eu gosto de fazer pizza.<br/><br/><br/>
+                Todo final de semana eu desço para Paranaguá, para matar a saudade da minha avó, minha tia e do meu pai (e do meu cachorro também).<br/><br/>
+
+                E à noite a gente assiste a alguma coisa, e para isso eu geralmente faço uma pizza; eu gosto de fazer pizza.<br/><br/><br/>
 
                 <ImageWithText src="/pizza.webp" description="Eu gosto de fazer pizza. :)"/>
             </Paragraph>
 
             <Title variant="orange">IFPR</Title>
             <Paragraph>
-                Como eu disse no vídeo, o IFPR pra mim foi um ponto de virada muito grande. Aquele lugar era estranho, e isso fazia dele perfeito.<br/><br/>
+                Como eu disse no vídeo, o IFPR para mim foi um ponto de virada muito grande. Aquele lugar era estranho, e isso fazia dele perfeito.<br/><br/>
 
-                No IFPR eu não tinha aulas normais de ensino médio que focavam em passar em vestibular, era uma ambiente mais livre, mais criativo.<br/><br/>
+                No IFPR eu não tinha aulas normais de ensino médio que focavam em passar em vestibular; era um ambiente mais livre, que me fazia sair da minha zona de conforto e estimulava mais a minha criatividade.<br/><br/>
 
-                Tem tanta coisa que eu tiver orgulho de participar no IFPR.<br/><br/>
+                Tem tanta coisa de que eu tenho orgulho de ter participado no IFPR.<br/><br/>
 
-                As aulas de artes e educação física eram as especialistas em fazer coisas aleatórias que me marcaram.<br/><br/>
+                As aulas de artes e educação física eram especialistas em fazer coisas aleatórias que me marcaram.<br/><br/>
 
-                Em educação física eu adorava que do nada a professora chegava e a gente tinha aula de dança, eu aprendi a dançar tango no IFPR.<br/><br/>
+                Em educação física, eu adorava quando, do nada, a professora chegava e a gente tinha aula de dança ou de luta; eu aprendi a dançar tango e boxe na mesma semana.<br/><br/>
 
-                Mas calma, vai melhorando, nas aulas de artes a gente teve que aprender a tocar uma música para apresentar em um festival, ninguém ali sabia tocar, mas a gente conseguiu.<br/><br/>
+                <ImageWithText src="/teatro.webp" description="Uma avaliação de artes era fazer um remake do Chaves."/>
 
-                Nos dois últimos anos, a gente precisava arrecadar dinheiro para nossa formatura, eu fazia parte da comissão e cuidava do financeiro. Para arrecadar fundos, a gente organizou um evento de anime no IF, veio muita gente, foi muito divertido e a gente conseguiu o dinheiro para fazer nossa formatura.<br/><br/>
+                Mas calma, vai melhorando. Nas aulas de artes, a gente teve que aprender a tocar uma música para apresentar em um festival. Ninguém ali sabia tocar, mas a gente conseguiu.<br/><br/>
 
-                No último ano teve o espetáculo, a gente teve que produzir um circo e apresentar para a escola inteira. O auditório tava lotado de gente e todo mundo no final gritava o nome da nossa turma.<br/><br/>
+                Nos dois últimos anos, a gente precisava arrecadar dinheiro para a nossa formatura. Eu fazia parte da comissão e cuidava do financeiro. Para arrecadar fundos, a gente organizou um evento de anime no IF; veio muita gente, foi muito divertido e a gente conseguiu o dinheiro para fazer nossa formatura.<br/><br/>
 
-                Também tinha eventos de e-sports no IFPR. Eu e meus amigos se juntaram e agente formou uma equipe de 6 pessoas. A equipe que eu participava ganhou o campeonato local e a gente ficou em 2º lugar no Paraná inteiro.<br/><br/>
+                No último ano teve o espetáculo; a gente teve que produzir um circo e apresentar para a escola inteira. O auditório estava lotado de gente e todo mundo, no final, gritava o nome da nossa turma.<br/><br/>
 
+                Também havia eventos de e-sports no IFPR. Eu e meus amigos nos juntamos e formamos uma equipe de 6 pessoas. A equipe que eu participava ganhou o campeonato local e a gente ficou em 2º lugar no Paraná inteiro.<br/><br/>
+
+                <ImageWithText src="/lol.webp" description="Jogando a final do campeonato local."/>
+                
+                E o principal, que eram as matérias técnicas, eu também gostei muito.<br/><br/>
+
+                Quando eu cheguei, consegui fazer amizades com os veteranos.<br/><br/>
+
+                Mais tarde, um amigo meu, que era veterano, me chamou para ser voluntário de um projeto que ele era bolsista.<br/><br/>
+
+                <ImageWithText src="/sisdle.webp" description="A gente apresentando o projeto na feira de cursos."/>
+
+                Eu aceitei, e foi algo muito bom para mim. Eu nunca tinha desenvolvido algo em equipe; no começo, eu não gostava que ele negasse minhas Pull Requests no GitHub, mas eu aprendi muita coisa com ele sobre desenvolvimento de software em equipe. Eu sempre busco passar essas orientações que recebi para frente. Quando eu pego um grupo mais inexperiente na faculdade, eu tento ser o que o meu amigo foi para mim lá atrás.<br/><br/>
+
+                Os frutos desse trabalho voluntário não pararam por aí. O professor desse projeto já me reconhecia por eu ser bom nas aulas dele. No ano seguinte, eu perguntei para ele se havia mais algum trabalho, porque eu tinha gostado de desenvolver. Foi então que surgiu a oportunidade de trabalhar na primeira Incubadora de Projetos do litoral do Paraná.<br/><br/>
+
+                <ImageWithText src="/incubadora.webp" description="Dia de visita do SEBRAE na Incubadora."/>
+            </Paragraph>
+
+            <Title variant="orange">Resumo da Ópera</Title>
+            <Paragraph>
+                Eu, Angelo Andrioli Netho, tenho 19 anos, gosto de natação, mas faço musculação, gosto de passar tempo de qualidade com minha família e meus amigos, gosto de fazer pizza, entre outras comidas, para as pessoas que eu gosto, tenho miopia e astigmatismo, amo meu cachorro, sou uma pessoa muito lógica, mas isso não me impede de ter uma inteligência emocional e criatividade bem desenvolvidas.
+                <ImageWithText src="/haru2.webp" description="Esse é o Haru, você vai ver bastante esse nome nos meus projetos."/>
             </Paragraph>
         </BaseWindow>
     )
